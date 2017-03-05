fdb63f01a69c885e23de80ce03f149a1ca2f5578
Andrew Wang <andrew@warden-website.localdomain>
2017-03-05 06:06:33 +0000

    Adding more to .gitignore and add simple test

---
+++ tests/test.py
@@ -0,0 +1,11 @@
+import unittest
+
+# TODO: Have better test cases
+
+class TestApp(unittest.TestCase):
+
+   def test_true(self):
+      self.assertTrue(True)
+
+if __name__ == '__main__':
+   unittest.main()
